4ac779d660a97131d35c544030484139965f29a8
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-11 20:51:46 -0400

    created and database

---
+++ models/tracker.js
@@ -0,0 +1,53 @@
+
+
+// Function to view all departments
+function viewAllDepartments() {
+    console.log('View all departments');
+    // Implement your code to fetch and display all departments from the database
+    startApp();
+  }
+  
+  // Function to view all roles
+  function viewAllRoles() {
+    console.log('View all roles');
+    // Implement your code to fetch and display all roles from the database
+    startApp();
+  }
+  
+  // Function to view all employees
+  function viewAllEmployees() {
+    console.log('View all employees');
+    // Implement your code to fetch and display all employees from the database
+    startApp();
+  }
+  
+  // Function to add a department
+  function addDepartment() {
+    console.log('Add a department');
+    // Implement your code to prompt the user for department details and add it to the database
+    startApp();
+  }
+  
+  // Function to add a role
+  function addRole() {
+    console.log('Add a role');
+    // Implement your code to prompt the user for role details and add it to the database
+    startApp();
+  }
+  
+  // Function to add an employee
+  function addEmployee() {
+    console.log('Add an employee');
+    // Implement your code to prompt the user for employee details and add it to the database
+    startApp();
+  }
+  
+  // Function to update an employee role
+  function updateEmployeeRole() {
+    console.log('Update an employee role');
+    // Implement your code to prompt the user for employee and role details and update it in the database
+    startApp();
+  }
+  
+  // Start the application
+  startApp();
