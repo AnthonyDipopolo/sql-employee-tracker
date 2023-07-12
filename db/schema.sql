0872cee7d3ec64dcb233938409340447b8d421c6
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-11 23:57:09 -0400

    fixed index.js errors and added manager column

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,13 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
+  manager_name VARCHAR(30),
+  department_name VARCHAR(30),
+  department_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
+
+-- INSERT INTO employee (manager_name)
+-- VALUES ("Stevens");
--- index.js
+++ index.js
@@ -1,73 +1,62 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-// const {
-//   viewAllDepartments,
-//   viewAllRoles,
-//   viewAllEmployees,
-//   addDepartment,
-//   addRole,
-//   addEmployee,
-//   updateEmployeeRole,
-// } = require('./models/tracker');
-
-// Function to start the application
-function userInput() {
-  inquirer.prompt({
-      name: 'action',
-      type: 'list',
-      message: 'What would you like to do?',
-      choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees',
-        'Add a department',
-        'Add a role',
-        'Add an employee',
-        'Update an employee role',
-        'Exit'
-      ]
-    })
-    .then(answer => {
-      switch (answer.action) {
-        case 'View all departments':
-          viewAllDepartments();
-          break;
-        case 'View all roles':
-          viewAllRoles();
-          break;
-        case 'View all employees':
-          viewAllEmployees();
-          break;
-        case 'Add a department':
-          addDepartment();
-          break;
-        case 'Add a role':
-          addRole();
-          break;
-        case 'Add an employee':
-          addEmployee();
-          break;
-        case 'Update an employee role':
-          updateEmployeeRole();
-          break;
-        case 'Exit':
-          console.log('Goodbye!');
-          break;
-        default:
-          console.log('Invalid option. Please try again.');
-          userInput();
-          break;
-      }
-    });
-}
-
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   database: 'employee_tracker_db'
 });
+
+// Function to start the application
+function userInput() {
+  inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit'
+    ]
+  }).then(answer => {
+    switch (answer.action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        console.log('Goodbye!');
+        break;
+      default:
+        console.log('Invalid option. Please try again.');
+        userInput();
+        break;
+    }
+  });
+}
 
 // Function to view all departments
 function viewAllDepartments() {
@@ -90,7 +79,12 @@ function viewAllRoles() {
   console.log('View all roles');
 
   // Execute a MySQL query to fetch all roles
-  connection.query('SELECT * FROM role', (err, results) => {
+  const query = `
+    SELECT role.id, role.title, role.salary, department.name AS department
+    FROM role
+    INNER JOIN department ON role.department_id = department.id
+  `;
+  connection.query(query, (err, results) => {
     if (err) throw err;
 
     // Display the fetched roles
@@ -107,11 +101,10 @@ function viewAllEmployees() {
 
   // Execute a MySQL query to fetch all employees with additional information from related tables
   const query = `
-    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_name AS manager
     FROM employee
     INNER JOIN role ON employee.role_id = role.id
-    INNER JOIN department ON role.department_id = department.id
-    LEFT JOIN employee manager ON employee.manager_id = manager.id
+    INNER JOIN department ON employee.department_id = department.id
   `;
   connection.query(query, (err, results) => {
     if (err) throw err;
@@ -184,42 +177,69 @@ function addRole() {
 
 // Function to add an employee
 function addEmployee() {
-  console.log('Add an employee');
-
-  // Prompt the user for employee details
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: 'Enter the employee first name:'
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: 'Enter the employee last name:'
-    },
-    {
-      type: 'number',
-      name: 'role_id',
-      message: 'Enter the role ID:'
-    },
-    {
-      type: 'number',
-      name: 'manager_id',
-      message: 'Enter the manager ID (if applicable):'
-    }
-  ]).then(answers => {
-    // Execute a MySQL query to insert the employee into the database
-    connection.query('INSERT INTO employee SET ?', answers, (err, result) => {
-      if (err) throw err;
-
-      console.log('Employee added successfully!');
-
-      // Return to the main menu
-      userInput();
+    console.log('Add an employee');
+  
+    // Prompt the user for employee details
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'Enter the employee first name:'
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'Enter the employee last name:'
+      },
+      {
+        type: 'number',
+        name: 'role_id',
+        message: 'Enter the role ID:'
+      },
+      {
+        type: 'input',
+        name: 'department_name',
+        message: 'Enter the department name:'
+      },
+      {
+        type: 'input',
+        name: 'manager_name',
+        message: 'Enter the manager name:'
+      }
+    ]).then(answers => {
+      // Retrieve the department ID based on the provided name
+      const { department_name, manager_name, ...employeeData } = answers;
+  
+      // Find the department ID
+      connection.query('SELECT id FROM department WHERE name = ?', department_name, (err, results) => {
+        if (err) throw err;
+  
+        if (results.length === 0) {
+          console.log(`Department '${department_name}' does not exist.`);
+          userInput();
+        } else {
+          const departmentId = results[0].id;
+  
+          // Set the department_id field in the employee data
+          employeeData.department_id = departmentId;
+  
+          // Set the manager_name field in the employee data
+          employeeData.manager_name = manager_name;
+  
+          // Execute a MySQL query to insert the employee into the database
+          connection.query('INSERT INTO employee SET ?', employeeData, (err, result) => {
+            if (err) throw err;
+  
+            console.log('Employee added successfully!');
+  
+            // Return to the main menu
+            userInput();
+          });
+        }
+      });
     });
-  });
-}
+  }
+  
 
 // Function to update an employee role
 function updateEmployeeRole() {
@@ -240,7 +260,7 @@ function updateEmployeeRole() {
       {
         type: 'number',
         name: 'role_id',
-        message: 'Enter the new role ID:'
+        message: 'Enter the new role ID number:'
       }
     ]).then(answers => {
       // Execute a MySQL query to update the employee's role
@@ -256,20 +276,5 @@ function updateEmployeeRole() {
   });
 }
 
-// module.exports = {
-//   viewAllDepartments,
-//   viewAllRoles,
-//   viewAllEmployees,
-//   addDepartment,
-//   addRole,
-//   addEmployee,
-//   updateEmployeeRole
-// };
-
-
 // Start the application
 userInput();
-
-// module.exports = {
-//     userInput
-// };
